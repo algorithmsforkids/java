2e7ec711760d68655d3794b54b8792c682e95f2d
Bunlong <bunlong.van@gmail.com>
2024-11-09 17:13:47 +0700

    Init

---
+++ data-structures/TheArrays.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class TheArrays {
+  public static void main(String[] args) {
+    String[] colors = new String[5];
+    colors[0] = "Purple";
+    colors[1] = "Blue";
+    
+    // System.out.println(Arrays.toString(colors));
+
+    // for (int i = 0; i < colors.length; i++) {
+    //   System.out.println(colors[i]);
+    // }
+
+    // for (String color : colors) {
+    //   System.out.println(color);
+    // }
+
+    Arrays.stream(colors).forEach(System.out::println);
+  }
+}
+
+// java TheArrays.java
