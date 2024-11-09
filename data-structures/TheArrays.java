db56e1c3205655afc8987a1dbf597a9f8e4f8ba6
Bunlong <bunlong.van@gmail.com>
2024-11-09 17:14:47 +0700

    TheArrays

--- data-structures/TheArrays.java
+++ data-structures/TheArrays.java
@@ -5,16 +5,16 @@ public class TheArrays {
     String[] colors = new String[5];
     colors[0] = "Purple";
     colors[1] = "Blue";
-    
-    // System.out.println(Arrays.toString(colors));
 
-    // for (int i = 0; i < colors.length; i++) {
-    //   System.out.println(colors[i]);
-    // }
+    System.out.println(Arrays.toString(colors));
 
-    // for (String color : colors) {
-    //   System.out.println(color);
-    // }
+    for (int i = 0; i < colors.length; i++) {
+      System.out.println(colors[i]);
+    }
+
+    for (String color : colors) {
+      System.out.println(color);
+    }
 
     Arrays.stream(colors).forEach(System.out::println);
   }
